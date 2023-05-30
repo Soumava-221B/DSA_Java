6dc7dacb22cd665d109cbef971def857bef71f19
Soumava Das <soumavadas06@gmail.com>
2023-05-30 16:09:31 +0000

    Linear and Binary

---
+++ linear_search.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class linear_search {
+    private static void LinearSearch(int[] arr, int item) {
+
+    for(int i=0; i<arr.length; i++) {
+        if(arr[i] == item) {
+            System.out.println(item + " Found at index: " + i);
+            return;
+        }
+    }
+        System.out.println("Not found");
+   }
+
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+
+         System.out.print("Enter the size of the array: ");
+         int arr_size = 0;
+         if (sc.hasNextInt()) {
+            arr_size = sc.nextInt();
+         }
+
+         int[] arr = new int[arr_size];
+
+         System.out.print("Enter the elements of the array: ");
+         for (int i = 0; i < arr_size; i++) {
+            if (sc.hasNextInt()) {
+                arr[i] = sc.nextInt();
+            }
+         }
+
+         System.out.print("Enter a number which you want to find in the array: ");
+         int item = sc.nextInt();
+
+         LinearSearch(arr, item);
+    }
+}
