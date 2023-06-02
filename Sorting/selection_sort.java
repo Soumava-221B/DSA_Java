d7ea4afcd0e878bfcae903999b4022d971c2228a
Soumava Das <soumavadas06@gmail.com>
2023-06-02 16:22:23 +0000

    merge and selection sort

---
+++ Sorting/selection_sort.java
@@ -0,0 +1,35 @@
+class selection_sort {
+    static void SelectionSort(int a[]) {
+        int len = a.length;
+        
+        for (int i = 0; i < len-1; i++)
+        {
+            int min = i;
+            for (int j=i+1; j<len; j++)
+            {
+                if (a[j] < a[min])
+                min = j;
+
+                int temp = a[min];
+                a[min] = a[i];
+                a[i] = temp;
+            }
+        }
+    }
+
+    static void printArray(int a[])
+    {
+        int len = a.length;
+        for (int i=0; i<len; i++)
+            System.out.print(a[i] + " ");
+
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {72, 50, 10, 44, 20};
+        SelectionSort(arr);
+        System.out.println("Sorted array");
+        printArray(arr);
+    }
+}
